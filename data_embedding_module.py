7d969fc1bdea85d5190170d4bcba8e6fc9a41a1c
안수범 <solseon0130@gmail.com>
2026-05-05 15:08:25 +0900

    update. 0430~0504 데이터 적재

--- data_embedding_module.py
+++ data_embedding_module.py
@@ -29,7 +29,7 @@ OPENAI_API_KEY =  os.getenv('Emb_KEY')
 MODEL_ID = "text-embedding-3-small"
 
 
-DB_HOST = "3.39.23.25"
+DB_HOST = "3.34.98.178"
 DB_PORT = "5432"  
 DB_USER = "rag"
 DB_PASSWORD = "rag"
